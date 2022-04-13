4a2c4c7b393fed03dc2cd7b4bb07447ef6ff0b59
UndERcaVEN <9189759110z@gmail.com>
2022-04-13 12:59:35 +0300

    виталя
    учись

---
+++ лаба22/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.errLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.avHLabel = new System.Windows.Forms.Label();
+            this.maxHLabel = new System.Windows.Forms.Label();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.writeButton = new System.Windows.Forms.Button();
+            this.readButton = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(725, 642);
+            this.listBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.errLabel);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.groupBox1.Location = new System.Drawing.Point(744, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(488, 191);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Добавление ученика";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(289, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "см";
+            // 
+            // errLabel
+            // 
+            this.errLabel.AutoSize = true;
+            this.errLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.errLabel.ForeColor = System.Drawing.Color.Red;
+            this.errLabel.Location = new System.Drawing.Point(103, 150);
+            this.errLabel.Name = "errLabel";
+            this.errLabel.Size = new System.Drawing.Size(270, 26);
+            this.errLabel.TabIndex = 5;
+            this.errLabel.Text = "Укажите данные ученика";
+            this.errLabel.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(6, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Рост ученика";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(6, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Имя ученика";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button1.Location = new System.Drawing.Point(325, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 74);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Добавить ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox2.Location = new System.Drawing.Point(165, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(118, 30);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox1.Location = new System.Drawing.Point(165, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 30);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.avHLabel);
+            this.groupBox2.Controls.Add(this.maxHLabel);
+            this.groupBox2.Controls.Add(this.refreshButton);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.groupBox2.Location = new System.Drawing.Point(744, 231);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(488, 189);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Данные о росте";
+            // 
+            // avHLabel
+            // 
+            this.avHLabel.AutoSize = true;
+            this.avHLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.avHLabel.Location = new System.Drawing.Point(196, 92);
+            this.avHLabel.Name = "avHLabel";
+            this.avHLabel.Size = new System.Drawing.Size(177, 25);
+            this.avHLabel.TabIndex = 9;
+            this.avHLabel.Text = "Наибольший рост";
+            this.avHLabel.Visible = false;
+            // 
+            // maxHLabel
+            // 
+            this.maxHLabel.AutoSize = true;
+            this.maxHLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.maxHLabel.Location = new System.Drawing.Point(196, 46);
+            this.maxHLabel.Name = "maxHLabel";
+            this.maxHLabel.Size = new System.Drawing.Size(177, 25);
+            this.maxHLabel.TabIndex = 8;
+            this.maxHLabel.Text = "Наибольший рост";
+            this.maxHLabel.Visible = false;
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.refreshButton.Location = new System.Drawing.Point(11, 138);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(157, 38);
+            this.refreshButton.TabIndex = 7;
+            this.refreshButton.Text = "Обновить ";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(6, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Средний рост";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(6, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(177, 25);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Наибольший рост";
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.deleteButton.Location = new System.Drawing.Point(11, 42);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(460, 38);
+            this.deleteButton.TabIndex = 10;
+            this.deleteButton.Text = "Удалить запись";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // writeButton
+            // 
+            this.writeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.writeButton.Location = new System.Drawing.Point(11, 150);
+            this.writeButton.Name = "writeButton";
+            this.writeButton.Size = new System.Drawing.Size(460, 38);
+            this.writeButton.TabIndex = 11;
+            this.writeButton.Text = "Записать в файл";
+            this.writeButton.UseVisualStyleBackColor = true;
+            this.writeButton.Click += new System.EventHandler(this.writeButton_Click);
+            // 
+            // readButton
+            // 
+            this.readButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.readButton.Location = new System.Drawing.Point(11, 97);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(460, 38);
+            this.readButton.TabIndex = 12;
+            this.readButton.Text = "Считать из файла";
+            this.readButton.UseVisualStyleBackColor = true;
+            this.readButton.Click += new System.EventHandler(this.readButton_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.readButton);
+            this.groupBox3.Controls.Add(this.deleteButton);
+            this.groupBox3.Controls.Add(this.writeButton);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.groupBox3.Location = new System.Drawing.Point(744, 436);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(488, 216);
+            this.groupBox3.TabIndex = 13;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Управление записями";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1244, 669);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label errLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label avHLabel;
+        private System.Windows.Forms.Label maxHLabel;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button writeButton;
+        private System.Windows.Forms.Button readButton;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ лаба22/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = textBox1.Text;
+                if (name == "") 
+                {
+                    throw new ArgumentNullException();
+                }
+                double height = Convert.ToDouble(textBox2.Text);
+                errLabel.Visible = false;
+
+                listBox1.Items.Add($"{name},\t {height}");
+            }
+            catch
+            {
+                errLabel.Visible = true;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            /*if (e.KeyChar >= '0' && e.KeyChar <= '9') return;
+            if (e.KeyChar == '.') e.KeyChar = ',';
+            if (e.KeyChar == ',')
+            {
+                if (textBox2.Text.IndexOf(',') != -1 || textBox2.Text.Length == 0)
+                    e.Handled = true;
+                return;
+            }
+
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter) button1.Focus();
+                else return;
+            }
+            e.Handled = true;*/
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            double maxH=0,s = 0;
+            string field;
+            string[] fields;
+            int hs=0;
+            
+            for (int i = 0; i < listBox1.Items.Count; ++i)
+            {
+                field = Convert.ToString(listBox1.Items[i]);
+
+                fields=field.Split(',');
+                s += Convert.ToDouble(fields[1]);
+                hs += Convert.ToInt32(fields[1]) + Convert.ToInt32(fields[2]);
+
+                if (Convert.ToDouble(fields[1]) > maxH)
+                {
+                    maxH = Convert.ToDouble(fields[1]);
+                }
+            }
+            s /= listBox1.Items.Count;
+            avHLabel.Text = Convert.ToString(hs);
+            maxHLabel.Text = Convert.ToString(maxH);
+
+            if(maxH>0)
+            {
+                maxHLabel.Visible = true;
+            }
+            if(s>0)
+            {
+                avHLabel.Visible = true;
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void writeButton_Click(object sender, EventArgs e)
+        {
+
+            StreamWriter streamwriter = new StreamWriter("list.txt", false);
+
+            for (int i = 0; i < listBox1.Items.Count; ++i)
+            {
+                streamwriter.WriteLine(listBox1.Items[i].ToString());
+            }
+            streamwriter.Flush();
+            streamwriter.Close();
+
+        }
+    }
+}
